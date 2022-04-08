849905eabdac02db52d65d515c3e1de694c8c8b3
mtojek <marcin.tojek@elastic.co>
2022-04-08 18:35:49 +0200

    WIP

--- interrupts/interrupts.s
+++ interrupts/interrupts.s
@@ -3,6 +3,11 @@ PORTA = $6001
 DDRB = $6002
 DDRA = $6003
 
+value = $0200 ; 2 bytes
+mod10 = $0202 ; 2 bytes
+message = $0204 ; 6 bytes
+counter = $020a ; 2 bytes
+
 E  = %10000000
 RW = %01000000
 RS = %00100000
@@ -12,6 +17,7 @@ RS = %00100000
 reset:
   ldx #$ff
   txs
+  cli
 
   lda #%11100000 ; Set top 3 pins on port A to output
   sta DDRA
@@ -19,22 +25,101 @@ reset:
   ; Wait until LCD init is done
   jsr lcd_wait
 
-  lda #%00001110 ; Display on/off: display on, cursor on, blink off
+  lda #%00001110 ; Display on; cursor on; blink off
+  jsr lcd_instruction
+  lda #%00000001 ; Clear display
   jsr lcd_instruction
 
-  ; Send characters
+  lda #0
+  sta counter
+  sta counter + 1
+
+loop:
+  lda #%00000010 ; Put cursor at home
+  jsr lcd_instruction
+
+  lda #0
+  sta message
+
+  ; Initialize value to be the number to convert
+  sei
+  lda counter
+  sta value
+  lda counter + 1
+  sta value + 1
+  cli
+
+divide:
+  ; Initialize the remainder to zero
+  lda #0
+  sta mod10
+  sta mod10 + 1
+  clc
+
+  ldx #16
+
+divloop:
+  ; Rotate quotient and remainder
+  rol value
+  rol value + 1
+  rol mod10
+  rol mod10 + 1
+
+  ; a,y = dividend - devisor
+  sec
+  lda mod10
+  sbc #10
+  tay ; save low byte in Y
+  lda mod10+1
+  sbc #0
+  bcc ignore_result ; branch if dividend < devisor
+  sty mod10
+  sta mod10 + 1
+
+ignore_result:
+  dex
+  bne divloop
+  rol value ; shift in the last bit of the quotient
+  rol value + 1
+
+  lda mod10
+  clc
+  adc #"0"
+  jsr  push_char
+
+  ; if value != 0, then continue dividing
+  lda value
+  ora value + 1
+  bne divide ; branch if value not equal to 0
+
   ldx #0
-send_characters:
+print:
   lda message,x
   beq loop
   jsr print_char
   inx
-  jmp send_characters
+  jmp print
 
-loop:
-  jmp loop
+; Add the character in the A register to the beginning of the 
+; null-terminated string `message`
+push_char:
+  pha ; Push new first char onto stack
+  ldy #0
 
-message: .asciiz "It works!!!"
+char_loop:
+  lda message,y ; Get char on the string and put into X
+  tax
+  pla
+  sta message,y ; Pull char off stack and add it to the string
+  iny
+  txa
+  pha           ; Push char from string onto stack
+  bne char_loop
+
+  pla
+  sta message,y ; Pull the null off the stack and add to the end of the string
+
+  rts
 
 lcd_wait:
   pha
@@ -46,7 +131,6 @@ lcdbusy:
   lda #(RW | E)
   sta PORTA
   lda PORTB
-
   and #%10000000
   bne lcdbusy
 
@@ -79,6 +163,8 @@ lcd_instruction:
 print_char:
   pha
 
+  jsr lcd_wait
+
   sta PORTB
 
   lda #RS        ; Clear RW/E bits
@@ -91,6 +177,15 @@ print_char:
   pla
   rts
 
-  .org $fffc
+irq:
+nmi:
+  inc counter
+  bne exit_nmi
+  inc counter + 1
+exit_nmi:
+  rti
+
+  .org $fffa
+  .word nmi
   .word reset
-  .word $0000
+  .word irq
